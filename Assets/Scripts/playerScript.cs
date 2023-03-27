f22c62139b5bc004bc9cd03d6ec4cd902fe2ab37
PC-imatge-2022-2023 <licenses@enti.cat>
2023-03-27 10:56:44 +0200

    Movimiento arreglado y camara

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -7,115 +7,31 @@ public class playerScript : MonoBehaviour
 
     float xInicial, yInicial;
 
-    public float speed;
-    public float jumpForce;
-
-    public Rigidbody2D rb;
-    public Animator anim;
-    public SpriteRenderer sr;
-
-    public Sprite jumpIMG;
-    public Sprite fallIMG;
-
-    bool canIdle = false;
-    bool canRun = true;
-
-
-    bool playerCanJump = true;
+    public float velocidadMovimiento;
+    private Rigidbody2D rb;
+    public float fuerzaSalto = 4.5f;
 
     void Start()
     {
-        anim = GetComponent<Animator>();
+
         rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
         xInicial = transform.position.x;
         yInicial = transform.position.y;
     }
 
     private void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        rb.AddForce(new Vector2(moveX, 0) * speed * Time.deltaTime);
-
-        if(moveY > 0 && playerCanJump)
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * velocidadMovimiento;
+        if (!Mathf.Approximately(0, movement))
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f)
         {
-            playerCanJump = false;
-            rb.AddForce(new Vector2(0, jumpForce));
-        }
-
-        if(rb.velocity.y > 0)
-        {
-            if(moveX > 0)
-            {
-                sr.flipX = false;
-            }
-            else if(moveX < 0)
-            {
-                sr.flipX = true;
-            }
-<<<<<<< HEAD
-=======
-            sr.sprite = jumpIMG;
->>>>>>> 1b12b4d73b9afde9776674217f76313a64c55254
-            anim.enabled = false;
-        }
-        else if(rb.velocity.y < 0)
-        {
-            if (moveX > 0)
-            {
-                sr.flipX = false;
-            }
-            else if (moveX < 0)
-            {
-                sr.flipX = true;
-            }
-<<<<<<< HEAD
-
-=======
-            sr.sprite = fallIMG;
->>>>>>> 1b12b4d73b9afde9776674217f76313a64c55254
-        }
-        else
-        {
-            anim.enabled = true;
-
-            if(moveX > 0 && canRun)
-            {
-                canRun = false;
-                canIdle = true;
-
-<<<<<<< HEAD
-=======
-                anim.SetTrigger("Corriendo");
->>>>>>> 1b12b4d73b9afde9776674217f76313a64c55254
-                sr.flipX = false;
-            }
-            else if(moveX < 0 && canRun)
-            {
-                canRun = false;
-                canIdle = true;
-
-<<<<<<< HEAD
-=======
-                anim.SetTrigger("Corriendo");
->>>>>>> 1b12b4d73b9afde9776674217f76313a64c55254
-                sr.flipX = true;
-            }
-            else if(moveX == 0 && canIdle)
-            {
-                canIdle = false;
-                canRun = true;
-
-            }
+            rb.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);
         }
     }
 
-    public void CanJump(bool tof)
-    {
-        playerCanJump = tof;
-    }
+   
 
     public void Recolocar()
     {
